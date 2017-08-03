9a29e2ace302406539c870635339db04240d562b
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-03 23:06:01 +0000

    grpc frontend seems to be working

--- grpc_client_py/clipper_grpc_client.py
+++ grpc_client_py/clipper_grpc_client.py
@@ -5,9 +5,22 @@ import time
 import numpy as np
 
 import grpc
+import logging
+import time
 
 import clipper_frontend_pb2
 import clipper_frontend_pb2_grpc
+
+from clipper_admin import Clipper
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%y-%m-%d:%H:%M:%S',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+input_type = "floats"
+app_name = "a1"
+model_name = "m1"
 
 
 # def guide_get_one_feature(stub, point):
@@ -26,15 +39,31 @@ import clipper_frontend_pb2_grpc
 #   guide_get_one_feature(stub, route_guide_pb2.Point(latitude=409146138, longitude=-746188906))
 #   guide_get_one_feature(stub, route_guide_pb2.Point(latitude=0, longitude=0))
 
+def setup():
+  clipper = Clipper("localhost", rpc_frontend=True, redis_port=6380)
+  clipper.start()
+  time.sleep(5)
+  clipper.register_application(app_name, input_type, "default_pred", 100000)
+
+  clipper.deploy_model(model_name,
+      1,
+      "/tmp/123456",
+      "clipper/noop-container:develop",
+      input_type,
+      num_containers=1)
+  clipper.link_model_to_app(app_name, model_name)
+
 
 def run():
   channel = grpc.insecure_channel('localhost:1337')
   stub = clipper_frontend_pb2_grpc.PredictStub(channel)
-  x = clipper_frontend_pb2.FloatsInput(input=list(np.random.random(100)))
-  req = clipper_frontend_pb2.PredictRequest(application="m1", input=x)
-  response = stub.PredictFloats(req)
-  print(response)
+  for _ in range(100):
+    x = clipper_frontend_pb2.FloatsInput(input=list(np.random.random(299*299*3)))
+    req = clipper_frontend_pb2.PredictRequest(application=app_name, input=x)
+    response = stub.PredictFloats(req)
+    print(response)
 
 
 if __name__ == '__main__':
+  # setup()
   run()
