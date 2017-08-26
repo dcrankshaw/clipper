f868a914f39ae5dfa374e1479d997a2e93a7d522
Corey Zumar <czumar@berkeley.edu>
2017-08-25 17:30:54 -0700

    grpc benchmark script

---
+++ grpc_client_py/grpc_benchmark.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+import grpc
+import logging
+import numpy as np
+import time
+
+import clipper_frontend_pb2
+import clipper_frontend_pb2_grpc
+
+from datetime import datetime
+
+from clipper_admin import Clipper
+
+DATA_TYPE_BYTES = 0
+DATA_TYPE_INTS = 1
+DATA_TYPE_FLOATS = 2
+DATA_TYPE_DOUBLES = 3
+DATA_TYPE_STRINGS = 4
+
+CIFAR_SIZE = 1024 * 8
+
+input_type = "doubles"
+app_name = "a1"
+model_name = "m1"
+
+def run():
+	channel = grpc.insecure_channel('localhost:1337')
+	stub = clipper_frontend_pb2_grpc.PredictStub(channel)
+	i = 0
+	while True:
+		begin = datetime.now()
+		x = clipper_frontend_pb2.FloatData(data=list(np.random.random(CIFAR_SIZE)))
+		req = clipper_frontend_pb2.PredictRequest(application=app_name, data_type=DATA_TYPE_FLOATS, float_data=x)
+		response = stub.Predict(req)
+		end = datetime.now()
+
+		latency += (end - begin).total_seconds()
+
+	if i % 10000 == 0:
+		print("Throughput: {} qps", float(latency) / i)
+		i = 0
+		latency = 0
+
+
