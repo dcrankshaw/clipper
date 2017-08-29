2e5d9b3f658513c5ffd0abab223b00198f86ad21
Corey Zumar <czumar@berkeley.edu>
2017-08-28 17:59:17 -0700

    update grpc benchmark

--- grpc_client_py/grpc_benchmark.py
+++ grpc_client_py/grpc_benchmark.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import sys
 import grpc
 import logging
 import numpy as np
@@ -11,18 +12,16 @@ import clipper_frontend_pb2_grpc
 from datetime import datetime
 from multiprocessing import Pool
 
-from clipper_admin import Clipper
-
 DATA_TYPE_BYTES = 0
 DATA_TYPE_INTS = 1
 DATA_TYPE_FLOATS = 2
 DATA_TYPE_DOUBLES = 3
 DATA_TYPE_STRINGS = 4
 
-CIFAR_SIZE = 3072
+CIFAR_SIZE = 384
 
 input_type = "doubles"
-app_name = "a1"
+app_name = "app1"
 model_name = "m1"
 
 def run():
@@ -47,11 +46,7 @@ if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		raise
 
-	num_procs = int(sys.argv[0])
+	num_procs = int(sys.argv[1])
 
 	pool = Pool(num_procs)
-	pool.map((run, None))
-
-
-
-
+	pool.map(run, [])
