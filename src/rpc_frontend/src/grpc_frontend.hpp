3d688ee449bb63dbdbee0c1d7aabe558c788e188
Corey Zumar <czumar@berkeley.edu>
2017-08-30 15:43:00 -0700

    test2

--- src/rpc_frontend/src/grpc_frontend.hpp
+++ src/rpc_frontend/src/grpc_frontend.hpp
@@ -461,15 +461,15 @@ class RequestHandler {
 
         request_throughput_->mark(1);
 
-        prediction.first.get();
-
-        PredictResponse &response = rpc_context->response_;
-        response.set_has_error(false);
-        std::shared_ptr<OutputData> output_data = prediction.second.output_.y_hat_;
-        response.set_data_type(static_cast<int>(output_data->type()));
-        response.set_default_explanation(prediction.second.default_explanation_.get_value_or(""));
-        response.set_is_default(prediction.second.output_is_default_);
-        rpc_context->send_response();
+        prediction.first.then([response = prediction.second](std::pair<size_t, folly::Try<folly::Unit>> output) {
+          PredictResponse &response = rpc_context->response_;
+          response.set_has_error(false);
+          std::shared_ptr<OutputData> output_data = prediction.second.output_.y_hat_;
+          response.set_data_type(static_cast<int>(output_data->type()));
+          response.set_default_explanation(prediction.second.default_explanation_.get_value_or(""));
+          response.set_is_default(prediction.second.output_is_default_);
+          rpc_context->send_response();
+        });
 
 //        prediction.first
 //          .then([app_metrics, rpc_context](Response r) {
