84dea17550441c9bb79219c40c8141e46929d1ab
Corey Zumar <czumar@berkeley.edu>
2017-09-19 17:22:17 -0700

    Client changes

--- zmq_client_py/clipper_zmq_client.py
+++ zmq_client_py/clipper_zmq_client.py
@@ -15,7 +15,6 @@ DATA_TYPE_FLOATS = 2
 DATA_TYPE_DOUBLES = 3
 DATA_TYPE_STRINGS = 4
 
-NUM_REQUESTS_SEND = 10
 NUM_RESPONSES_RECV = 10
 
 class Client:
@@ -23,18 +22,28 @@ class Client:
 	active = False
 
 	def __init__(self, clipper_host, clipper_send_port, clipper_recv_port):
+		"""
+		Parameters
+		----------
+		clipper_host : str
+			The address of an active clipper host
+		clipper_send_port : int
+			The clipper port to which we will send requests
+		clipper_recv_port : int
+			The clipper port from which we will receive responses		
+		"""
 		self.clipper_host = clipper_host
 		self.send_port = clipper_send_port
 		self.recv_port = clipper_recv_port
 		self.request_queue = Queue()
-		self.recv_count = 0
 
 	def start(self):
 		global active
 		active = True
-		self.thread = Thread(target=self._run, args=[])
-		self.start_time = datetime.now()
-		self.thread.start()
+		self.recv_thread = Thread(target=self._run_recv, args=[])
+		self.recv_thread.start()
+		self.send_thread = Thread(target=self._run_send, args=[])
+		self.send_thread.start()
 
 	def stop(self):
 		global active
@@ -42,58 +51,69 @@ class Client:
 			active = False
 			self.thread.join()
 
-	def send_request(self, app_name, input_item):
+	def send_request(self, app_name, input_item, callback=None):
 		self.request_queue.put((app_name, input_item))
 
-	def _run(self):
+	def _run_recv(self):
 		global active
+		# The address of the socket from which we want to receive data
 		clipper_recv_address = "tcp://{0}:{1}".format(self.clipper_host, self.recv_port)
-		clipper_send_address = "tcp://{0}:{1}".format(self.clipper_host, self.send_port)
-		context = zmq.Context(2)
-		recv_socket = context.socket(zmq.DEALER)
-		send_socket = context.socket(zmq.DEALER)
-
+		context = zmq.Context()
+		socket = context.socket(zmq.DEALER)
 		poller = zmq.Poller()
-		poller.register(recv_socket, zmq.POLLIN)
-		poller.register(send_socket, zmq.POLLIN)
+		poller.register(socket, zmq.POLLIN)
 
-		recv_socket.connect(clipper_recv_address)
-		send_socket.connect(clipper_send_address)
+		socket.connect(clipper_recv_address)
+		# Send a blank message to establish a connection
+		socket.send("", ZMQ.SNDMORE)
+		socket.send("")
+		connected = False
 		while active:
-			if self.request_queue.empty():
-				receivable_sockets = dict(poller.poll(1000))
-				if socket in receivable_sockets and receivable_sockets[socket] == zmq.POLLIN:
+			timeout = 1000 if connected else 5000
+			receivable_sockets = dict(poller.poll(timeout))
+			if socket in receivable_sockets and receivable_sockets[socket] == zmq.POLLIN:
+				if connected:
 					self._receive_response(socket)
-					for i in range(NUM_REQUESTS_SEND - 1):
-						receivable_sockets = dict(poller.poll(0))
-						if socket in receivable_sockets and receivable_sockets[socket] == zmq.POLLIN:
-							self._receive_response(socket)
-			else:
-				for i in range(NUM_RESPONSES_RECV):
-					receivable_sockets = dict(poller.poll(1000))
-					if socket in receivable_sockets and receivable_sockets[socket] == zmq.POLLIN:
-						self._receive_response(socket)
+				else:
+					self._handle_new_connection(socket)
+					connected = True
 
-			self._send_requests(socket)
+	def _run_send(self):
+		global active
+		# The address of the socket to which we want to send data
+		clipper_send_address = "tcp://{0}:{1}".format(self.clipper_host, self.send_port)
+		context = zmq.Context()
+		socket = context.socket(zmq.DEALER)
+		poller.register(socket, zmq.POLLIN)
+
+		socket.connect(clipper_send_address)
+		while active:
+			if not self.client_id:
+				time.sleep(1000)
+			else:
+				self._send_requests(socket)
+
+	def _handle_new_connection(self, socket):
+		# Receive delimiter between routing identity and content
+		socket.recv()
+		client_id_bytes = socket.recv()
+		self.client_id = struct.unpack("<I", client_id_bytes)
 
 	def _receive_response(self, socket):
 		# Receive delimiter between routing identity and content
 		socket.recv()
 		data_type_bytes = socket.recv()
 		output_data = socket.recv()
-		self.recv_count += 1
-		if self.recv_count % 200 == 0:
-			curr_time = datetime.now()
-			latency = (curr_time - self.start_time).total_seconds()
-			print("Throughput: {} qps\n".format(200.0 / latency))
-			self.start_time = curr_time
-
+		print("RECEIVED RESPONSE!")
 
 	def _send_requests(self, socket):
-		i = NUM_REQUESTS_SEND
+		if self.request_queue.empty():
+			time.sleep(1000)
+
 		while (not self.request_queue.empty()) and i > 0:
 			app_name, input_item = self.request_queue.get()
 			socket.send("", zmq.SNDMORE)
+			socket.send(struct.pack("<I", self.client_id), zmq.SNDMORE)
 			socket.send_string(app_name, zmq.SNDMORE)
 			socket.send(struct.pack("<I", DATA_TYPE_DOUBLES), zmq.SNDMORE)
 			socket.send(struct.pack("<I", len(input_item)), zmq.SNDMORE)
