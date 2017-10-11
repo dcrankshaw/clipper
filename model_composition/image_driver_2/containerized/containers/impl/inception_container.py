416d6fdb49eb81e4efdae76625cacda87862ff29
Corey Zumar <czumar@berkeley.edu>
2017-10-10 19:01:11 -0700

    path fixes

--- model_composition/image_driver_2/containerized/containers/impl/inception_container.py
+++ model_composition/image_driver_2/containerized/containers/impl/inception_container.py
@@ -7,7 +7,7 @@ import base64
 import tensorflow as tf
 import numpy as np
 
-sys.path.append("/tensorflow/models/research/slim")
+sys.path.append("/tf_models/research/slim")
 
 from nets import inception_v3
 from preprocessing import inception_preprocessing
