58b4568eb5bfc1f2ec2ef2ad5e0f04d612104e21
Corey Zumar <czumar@berkeley.edu>
2017-10-20 00:04:42 -0700

    CVD export

--- model_composition/image_driver_1/single_process/multi_proc_driver.sh
+++ model_composition/image_driver_1/single_process/multi_proc_driver.sh
@@ -15,23 +15,27 @@ NUM_PROCS=$1
 if [ "$NUM_PROCS" -ge "1" ]
 	then
 		echo '1'
+		export CUDA_VISIBLE_DEVICES=0,1
 		numactl -C 1,2,3,4 python driver.py -c 1 2 3 4 -v 0 -i 1 -b 32 &
 fi
 
 if [ "$NUM_PROCS" -ge "2" ]
 	then
 		echo '2'
+		export CUDA_VISIBLE_DEVICES=2,3
 		numactl -C 5,6,7,8 python driver.py -c 5 6 7 8 -v 2 -i 3 -b 32 &
 fi
 
 if [ "$NUM_PROCS" -ge "3" ]
 	then
 		echo '3'
+		export CUDA_VISIBLE_DEVICES=4,5
 		numactl -C 9,10,11,12 python driver.py -c 9 10 11 12 -v 4 -i 5 -b 32 &
 fi
 
 if [ "$NUM_PROCS" -ge "4" ]
 	then
 		echo '4'
+		export CUDA_VISIBLE_DEVICES=6,7
 		numactl -C 13,14,15,16 python driver.py -c 13 14 15 16 -v 6 -i 7 -b 32 &
 fi
