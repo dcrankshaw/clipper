cde7909c07432541c47b97c176afd457f690910b
Corey Zumar <czumar@berkeley.edu>
2017-10-21 16:09:52 -0700

    build script fix

--- model_composition/image_driver_1/containerized/containers/build_docker_images.sh
+++ model_composition/image_driver_1/containerized/containers/build_docker_images.sh
@@ -20,7 +20,7 @@ cd $DIR
 # Build model-specific images
 time docker build -t model-comp/tf-kernel-svm -f TfKernelSvmDockerfile ./
 time docker build -t model-comp/tf-resnet-feats -f TfResNetDockerfile ./
-time docker build -t model-comp/tf-log-reg -f TfLogisticRegressionDockerfile./
+time docker build -t model-comp/tf-log-reg -f TfLogisticRegressionDockerfile ./
 time docker build -t model-comp/vgg-feats -f VggFeaturizationDockerfile ./
 time docker build -t model-comp/kpca-svm -f VggKpcaSvmDockerfile ./
 time docker build -t model-comp/kernel-svm -f VggKernelSvmDockerfile ./
