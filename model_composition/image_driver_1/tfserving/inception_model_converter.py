500e27bd9f41a40513ad9afd3fb1db62671ff3ff
Dan Crankshaw <dscrankshaw@gmail.com>
2017-10-22 07:38:26 +0000

    started model conversion scripts for tfserving

---
+++ model_composition/image_driver_1/tfserving/inception_model_converter.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import os
+import tensorflow as tf
+
+
+def export():
+    inception_model_path = os.path.abspath("./raw_data/inception_feats_graph_def.pb")
+    output_path = os.path.join(os.path.abspath("./exported_tf_models/inception_tfserve/1"),
+                               "inception_tfserve.pb")
+    with tf.Session() as sess:
+        with open(inception_model_path, mode='rb') as inception_file:
+            inception_text = inception_file.read()
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(inception_text)
+
+        inputs_tensor = tf.placeholder(tf.float32, shape=[None, 299, 299, 3])
+        tf.import_graph_def(graph_def, name='', input_map={"ResizeBilinear:0": inputs_tensor})
+        feats_tensor = tf.get_default_graph().get_tensor_by_name("pool_3:0")
+
+        builder = tf.saved_model.builder.SavedModelBuilder(output_path)
+
+        predict_inputs_tensor_info = tf.saved_model.utils.build_tensor_info(inputs_tensor)
+        feats_output_tensor_info = tf.saved_model.utils.build_tensor_info(feats_tensor)
+
+        prediction_signature = (
+            tf.saved_model.signature_def_utils.build_signature_def(
+                inputs={'inputs': predict_inputs_tensor_info},
+                outputs={
+                    'feats': feats_output_tensor_info
+                },
+                method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME
+            ))
+
+        builder.add_meta_graph_and_variables(
+            sess, [tf.saved_model.tag_constants.SERVING],
+            signature_def_map={
+                'predict_inputs':
+                    prediction_signature
+            })
+
+        builder.save()
+
+
+if __name__ == "__main__":
+    export()
---
+++ model_composition/image_driver_1/tfserving/tfserving_client.py
@@ -0,0 +1,152 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+"""Send JPEG image to tensorflow_model_server loaded with inception model.
+"""
+
+from __future__ import print_function
+
+import os
+import json
+import numpy as np
+# from io import BytesIO
+# from PIL import Image
+import logging
+from datetime import datetime
+from grpc.beta import implementations
+import tensorflow as tf
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+from multiprocessing import Process, Queue
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%y-%m-%d:%H:%M:%S',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def gen_imagenet_image():
+
+    input_img = np.array(np.random.rand(299, 299, 3) * 255, dtype=np.float32)
+    return input_img
+    # input_img = Image.fromarray(input_img.astype(np.uint8))
+    # inmem_inception_jpeg = BytesIO()
+    # resized_inception = input_img.resize((299, 299)).convert('RGB')
+    # resized_inception.save(inmem_inception_jpeg, format="JPEG")
+    # inmem_inception_jpeg.seek(0)
+    # data = inmem_inception_jpeg.read()
+    # return data
+
+
+def run_benchmark(q):
+    bench = Benchmarker(q)
+    bench.run()
+
+
+class Benchmarker(object):
+    def __init__(self, queue):
+        host = "localhost"
+        port = 9000
+        self.channel = implementations.insecure_channel(host, int(port))
+        self.stub = prediction_service_pb2.beta_create_PredictionService_stub(self.channel)
+        self.images = [gen_imagenet_image() for _ in range(100)]
+        self.queue = queue
+        self.stats = {
+            "thrus": [],
+            "p99_lats": [],
+            "mean_lats": []}
+
+    def init_stats(self):
+        self.latencies = []
+        self.batch_num_complete = 0
+        self.start_time = datetime.now()
+
+    def print_stats(self):
+        lats = np.array(self.latencies)
+        p99 = np.percentile(lats, 99)
+        mean = np.mean(lats)
+        end_time = datetime.now()
+        thru = float(self.batch_num_complete) / (end_time - self.start_time).total_seconds()
+        self.stats["thrus"].append(thru)
+        self.stats["p99_lats"].append(p99)
+        self.stats["mean_lats"].append(mean)
+        logger.info("p99: {p99}, mean: {mean}, thruput: {thru}".format(p99=p99,
+                                                                       mean=mean,
+                                                                       thru=thru))
+
+    def run(self, num_trials=7, reqs_per_trial=100):
+        self.init_stats()
+        for t in range(num_trials):
+            for r in range(reqs_per_trial):
+                data = self.images[r % len(self.images)]
+                request = predict_pb2.PredictRequest()
+                request.model_spec.name = 'inception_feats'
+                request.model_spec.signature_name = 'predict_inputs'
+                # TODO: remove this copy
+                request.inputs['inputs'].CopyFrom(
+                    tf.contrib.util.make_tensor_proto(data, shape=[1, 299, 299, 3]))
+                rstart = datetime.now()
+                result = self.stub.Predict(request, 5.0)
+                # print(result.outputs["feats"])
+                # return
+                lat = (datetime.now() - rstart).total_seconds()
+                self.latencies.append(lat)
+                self.batch_num_complete += 1
+            self.print_stats()
+            self.init_stats()
+            # logger.info("Completed {} of {} trials".format(t, num_trials))
+        self.queue.put(self.stats)
+
+
+def mean_throughput(stats):
+    mean_thru_per_client = [np.mean(s["thrus"][1:]) for s in stats]
+    var_thru_per_client = [np.var(s["thrus"][1:]) for s in stats]
+    mean_total = np.sum(mean_thru_per_client)
+    std_total = np.sqrt(np.sum(var_thru_per_client))
+    return (mean_total, std_total)
+
+
+if __name__ == '__main__':
+
+    # for num_procs in [1, 5, 10, 15, 20]:
+    num_procs = 5
+    q = Queue()
+    processes = []
+    stats = []
+    for _ in range(num_procs):
+        p = Process(target=run_benchmark, args=(q,))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        stats.append(q.get())
+        p.join()
+
+    results_dir = os.path.abspath("results")
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+
+    results_file = os.path.join(results_dir,
+                                "{procs}-clients_{time:%y%m%d_%H%M%S}.json".format(
+                                    procs=num_procs, time=datetime.now()))
+
+    with open(results_file, "w") as f:
+        json.dump(stats, f, indent=4)
+        logger.info("Saved results to {}".format(results_file))
+        mean_total, std_total = mean_throughput(stats)
+    logger.info("Num clients: {}, mean throughput: {} +- {}".format(
+        num_procs, mean_total, std_total))
