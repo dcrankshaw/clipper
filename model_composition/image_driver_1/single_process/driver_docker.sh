312c99c6f7609ec5ba461f3cbfadf26313df7adc
Corey Zumar <czumar@berkeley.edu>
2017-10-26 02:59:29 -0700

    fix

--- model_composition/image_driver_1/single_process/driver_docker.sh
+++ model_composition/image_driver_1/single_process/driver_docker.sh
@@ -12,4 +12,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 [ -d ${DIR}/results ] || mkdir ${DIR}/results
 
-nvidia-docker run -d -v ${DIR}/results:/results model-comp/sp-img-driver-1
+nvidia-docker run -d -v ${DIR}/results:/results model-comp/sp-img-driver-1 -t 12 -b 1 2 4 6 8 10 16 24 32 48 64 -c 17 -r 0 -i 1 -tl 100
