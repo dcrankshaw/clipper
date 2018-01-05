cbc1a00928bf545715fd79ad67af0472b99b36fb
Corey Zumar <czumar@berkeley.edu>
2018-01-05 14:16:27 -0800

    Min lat max thru gpu fixes

--- model_composition/image_driver_1/single_process/min_lat.sh
+++ model_composition/image_driver_1/single_process/min_lat.sh
@@ -21,17 +21,17 @@ fi
 if [ "$NUM_PROCS" -ge "2" ]
 	then
 		echo '2'
-		(export CUDA_VISIBLE_DEVICES=0,1; numactl -C 4,20,5,21,6,22,7,23 python driver.py -c 4 20 5 21 6 22 7 23 -r 0 -i 1 -b 1 -t 8 -tl 200) &
+		(export CUDA_VISIBLE_DEVICES=2,3; numactl -C 4,20,5,21,6,22,7,23 python driver.py -c 4 20 5 21 6 22 7 23 -r 0 -i 1 -b 1 -t 8 -tl 200) &
 fi
 
 if [ "$NUM_PROCS" -ge "3" ]
 	then
 		echo '3'
-		(export CUDA_VISIBLE_DEVICES=0,1; numactl -C 8,24,9,25,10,26,11,27 python driver.py -c 8 24 9 25 10 26 11 27 -r 0 -i 1 -b 1 -t 8 -tl 200) &
+		(export CUDA_VISIBLE_DEVICES=4,5; numactl -C 8,24,9,25,10,26,11,27 python driver.py -c 8 24 9 25 10 26 11 27 -r 0 -i 1 -b 1 -t 8 -tl 200) &
 fi
 
 if [ "$NUM_PROCS" -ge "4" ]
 	then
 		echo '4'
-		(export CUDA_VISIBLE_DEVICES=0,1; numactl -C 12,28,13,29,14,30,15,31 python driver.py -c 12 28 13 29 14 30 15 31 -r 0 -i 1 -b 1 -t 8 -tl 200) &
+		(export CUDA_VISIBLE_DEVICES=6,7; numactl -C 12,28,13,29,14,30,15,31 python driver.py -c 12 28 13 29 14 30 15 31 -r 0 -i 1 -b 1 -t 8 -tl 200) &
 fi
