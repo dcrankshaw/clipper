0d908d05d79d4bb7e95994d9e7fc8ea9902f42e7
Corey Zumar <czumar@berkeley.edu>
2018-02-05 16:33:33 -0800

    docker build script fixes

--- model_composition/image_driver_1/containerized/containers/build_docker_images.sh
+++ model_composition/image_driver_1/containerized/containers/build_docker_images.sh
@@ -13,8 +13,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Build RPC base images for python/anaconda and deep learning
 # models
 cd $DIR/../../../container_utils/
-#time docker build -t model-comp/py-rpc -f RpcDockerfile ./
-#time docker build -t model-comp/tf-rpc -f TfRpcDockerfile ./
+time docker build -t model-comp/py-rpc -f RpcDockerfile ./
+time docker build -t model-comp/tf-rpc -f TfRpcDockerfile ./
 time docker build -t model-comp/cuda-rpc -f CudaPyRpcDockerfile ./
 
 cd $DIR
