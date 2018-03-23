3f1eff6257735eac681e9db38011bdbd1041671a
Ubuntu <ubuntu@ip-172-31-8-155.us-west-2.compute.internal>
2018-03-23 02:55:31 +0000

    Text-driver-1 containers dir fixes

--- model_composition/text-driver-1/containerized/containers/build_docker_images.sh
+++ model_composition/text-driver-1/containerized/containers/build_docker_images.sh
@@ -10,15 +10,25 @@ unset CDPATH
 # the script.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+gcpprefix="gcr.io/clipper-model-comp"
+tag="bench"
+
 # Build RPC base images for python/anaconda and deep learning
 # models
-cd $DIR/../../../container_utils/
-time docker build -t model-comp/theano-rpc -f TheanoRpcDockerfile ./
+cd /home/ubuntu/clipper/model_composition/container_utils 
 time docker build -t model-comp/tf-rpc -f TfRpcDockerfile ./
 time docker build -t model-comp/tf-1-4-rpc -f Tf1-4RpcDockerfile ./
 
 cd $DIR
 # Build model-specific images
-time docker build -t model-comp/theano-lstm -f TheanoSentimentDockerfile ./
-time docker build -t model-comp/tf-autocomplete -f TfAutoCompleteDockerfile ./
-time docker build -t model-comp/nmt -f NmtDockerfile ./
+time docker build -t model-comp/tf-lang-detect -f TfLangDetectionDockerfile ./
+docker tag model-comp/tf-lang-detect $gcpprefix/tf-lang-detect:$tag
+# gcloud docker -- push $gcpprefix/tf-lang-detect:$tag
+
+time docker build -t model-comp/tf-lstm -f TfLstmDockerfile ./
+docker tag model-comp/tf-lstm $gcpprefix/tf-lstm:$tag
+# gcloud docker -- push $gcpprefix/tf-lstm:$tag
+
+time docker build -t model-comp/tf-nmt -f NmtDockerfile ./
+docker tag model-comp/tf-nmt $gcpprefix/tf-nmt:$tag
+# gcloud docker -- push $gcpprefix/tf-nmt:$tag
