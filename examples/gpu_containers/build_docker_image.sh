806c8d9ad27041ab81e124c0c5cda9c429e0b5ca
Dan Crankshaw <dscrankshaw@gmail.com>
2018-04-02 16:41:36 -0700

    started adding gpu example

---
+++ examples/gpu_containers/build_docker_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+unset CDPATH
+# one-liner from http://stackoverflow.com/a/246128
+# Determines absolute path of the directory containing
+# the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Let the user start this script from anywhere in the filesystem.
+CLIPPER_ROOT=$DIR/../..
+cd $CLIPPER_ROOT
+
+docker build -f examples/gpu_containers/Dockerfile -t clipper/pytorch-container:gpu .
