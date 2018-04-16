40cc02dc2945a28bcd935241317e4bb5ddb9f9a0
Dan Crankshaw <dscrankshaw@gmail.com>
2018-04-16 19:36:10 +0000

    gcp changes

--- model_composition/image_driver_1/containerized/containers/build_docker_images.sh
+++ model_composition/image_driver_1/containerized/containers/build_docker_images.sh
@@ -16,19 +16,20 @@ tag="bench"
 # Build RPC base images for python/anaconda and deep learning
 # models
 cd $DIR/../../../container_utils/
-time docker build -t model-comp/py-rpc -f RpcDockerfile ./
+# time docker build -t model-comp/py-rpc -f RpcDockerfile ./
 time docker build -t model-comp/tf-rpc -f TfRpcDockerfile ./
-time docker build -t model-comp/cuda-rpc -f CudaPyRpcDockerfile ./
+time docker build -t model-comp/tf-rpc-nogpu -f TfRpcNoGpuDockerfile ./
+# time docker build -t model-comp/cuda-rpc -f CudaPyRpcDockerfile ./
 
 cd $DIR
 # Build model-specific images
 time docker build -t model-comp/tf-kernel-svm -f TfKernelSvmDockerfile ./
 docker tag model-comp/tf-kernel-svm $gcpprefix/tf-kernel-svm:$tag
-# gcloud docker -- push $gcpprefix/tf-kernel-svm:$tag
+gcloud docker -- push $gcpprefix/tf-kernel-svm:$tag
 
 time docker build -t model-comp/tf-resnet-feats -f TfResNetDockerfile ./
 docker tag model-comp/tf-resnet-feats $gcpprefix/tf-resnet-feats:$tag
-# gcloud docker -- push $gcpprefix/tf-resnet-feats:$tag
+gcloud docker -- push $gcpprefix/tf-resnet-feats:$tag
 
 # time docker build -t model-comp/tf-resnet-feats-variable-input -f TfResNetVariableInputSizeDockerfile ./
 # docker tag model-comp/tf-resnet-feats-variable-input $gcpprefix/tf-resnet-feats-variable-input:$tag
@@ -40,8 +41,8 @@ docker tag model-comp/tf-resnet-feats $gcpprefix/tf-resnet-feats:$tag
 
 time docker build -t model-comp/tf-log-reg -f TfLogisticRegressionDockerfile ./
 docker tag model-comp/tf-log-reg $gcpprefix/tf-log-reg:$tag
-# gcloud docker -- push $gcpprefix/tf-log-reg:$tag
+gcloud docker -- push $gcpprefix/tf-log-reg:$tag
 
 time docker build -t model-comp/inception-feats -f InceptionFeaturizationDockerfile ./
 docker tag model-comp/inception-feats $gcpprefix/inception-feats:$tag
-# gcloud docker -- push $gcpprefix/inception-feats:$tag
+gcloud docker -- push $gcpprefix/inception-feats:$tag
